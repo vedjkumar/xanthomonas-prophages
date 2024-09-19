6d8aab13b28abdb0d05e2f2a762dbeccb25483de
Ved Jan Kumar <kumarvedjan@gmail.com>
2024-09-19 19:42:17 +0100

    Update inclusion.py

--- ASM_lists/inclusion.py
+++ ASM_lists/inclusion.py
@@ -1,1 +1,13 @@
 # Sets out inclusion criteria for assemblies
+
+from pathlib import Path
+import pandas as pd
+
+accepted = pd.DataFrame(columns=["Assembly Name", "Organism Name", "Organism Intraspecific Names Strain", "Assembly Level", "Assembly Release Date", "Assembly Sequencing Tech", "CheckM completeness", "CheckM contamination"])
+
+dir = "ASM_lists"
+files = Path(dir).glob("*.tsv")
+for file in files:
+  
+  with open(file) as f:
+    #
